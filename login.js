d4690e779ed4443d06a6ea6035e172b1ca6b77e3
huangyulong <huangyl@lhcis.com>
2024-10-21 17:07:02 +0800

    pages

---
+++ login.js
@@ -0,0 +1,23 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+
+});
+
+function requestFullScreen(element) {
+	var element=document.documentElement;
+	var requestMethod = element.requestFullScreen || //W3C
+	element.webkitRequestFullScreen || //Chrome等
+	element.mozRequestFullScreen || //FireFox
+	element.msRequestFullScreen; //IE11
+	if (requestMethod) {
+		requestMethod.call(element);
+	}
+	else if (typeof window.ActiveXObject !== "undefined") {//for Internet Explorer
+		var wscript = new ActiveXObject("WScript.Shell");
+		if (wscript !== null) {
+		 wscript.SendKeys("{F11}");
+		}
+	}
+}
